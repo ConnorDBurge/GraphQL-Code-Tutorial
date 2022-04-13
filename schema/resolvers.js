e35c950623717ca35387bf745594d536eaf6823f
ConnorDBurge <connor.d.burge@gmail.com>
2022-04-12 18:28:36 -0700

    created first query

---
+++ schema/resolvers.js
@@ -0,0 +1,14 @@
+const { UsersList } = require('../FakeData')
+
+const resolvers = {
+
+    Query: {
+        users() {
+            return UsersList;
+        }
+    }
+}
+
+module.exports = {
+    resolvers
+}
